48a7b3a4003fd8e29131641d3a5f4ccf9d5e8c50
carriere4 <carriere4@gmail.com>
2017-10-06 14:55:24 -0500

    first stab at automate js text via py

--- hfjavascript/create_code.py
+++ hfjavascript/create_code.py
@@ -6,3 +6,6 @@ for i in range(7):
 
 #list comprehension
 [str(i) + " / " + str(j) for i in range(6) for j in range(6)]
+
+['<td id="'+str(i)+str(j)+'"></td>' for i in range(6) for j in range(6)]
+#need to find a way to get single quotes out of result
